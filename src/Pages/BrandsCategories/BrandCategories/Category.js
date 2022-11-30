5b11b9efe3aaf38dc4ee942340d48378f91010a5
Rabbe <onikmahmudrabbe@gmail.com>
2022-11-30 17:31:00 +0600

    favicon added

--- src/Pages/BrandsCategories/BrandCategories/Category.js
+++ src/Pages/BrandsCategories/BrandCategories/Category.js
@@ -7,11 +7,11 @@ const Category = ({ brandCategory }) => {
         <Link to={`categories/${_id}`}>
             <div className="card w-96 bg-base-100 shadow-xl">
                 <figure className="px-10 pt-10">
-                    <img src={image} alt="Shoes" className="rounded-xl" />
+                    <img src={image} alt="Shoes" className="rounded-xl h-[180px]" />
                 </figure>
                 <div className="card-body items-center text-center">
                     <h2 className="card-title">{brandName}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>Your Dream is here. Use it to make life more easier.</p>
                     <div className="card-actions">
                         <button className="btn btn-primary">Show Products</button>
                     </div>
