2c93058f755884beea91b186dc9cd4553940109e
Rabbe <onikmahmudrabbe@gmail.com>
2022-12-02 02:38:11 +0600

    last update

--- src/Pages/BrandsCategories/BrandCategories/Category.js
+++ src/Pages/BrandsCategories/BrandCategories/Category.js
@@ -5,13 +5,13 @@ const Category = ({ brandCategory }) => {
     const { brandName, image, _id } = brandCategory;
     return (
         <Link to={`/categories/${brandName}`}>
-            <div className="card w-96 bg-slate-400 rounded-md shadow-xl">
+            <div className="card w-96 bg-slate-300 rounded-md shadow-xl">
                 <figure className="px-10 pt-10">
                     <img src={image} alt="Shoes" className="rounded-xl h-[180px]" />
                 </figure>
                 <div className="card-body items-center text-center">
-                    <h2 className="card-title">{brandName}</h2>
-                    <p>Your Dream is here. Use it to make life more easier.</p>
+                    <h2 className="card-title text-white">{brandName}</h2>
+                    <p className='text-white'>Your Dream is here. Use it to make life more easier.</p>
                     <div className="card-actions">
                         <button className="btn btn-primary">Show Products</button>
                     </div>
