e42ef5aa1ced73725ff5816b7d9439522c67589a
Rabbe <onikmahmudrabbe@gmail.com>
2022-12-22 20:32:12 +0600

    navigate to login added

--- src/Pages/BrandsCategories/BrandProducts/ProductCard.js
+++ src/Pages/BrandsCategories/BrandProducts/ProductCard.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
 
 const ProductCard = ({ product, setOrders }) => {
     const { name, location, Image, originalPrice, resalePrice, use } = product;
+    const { user } = useContext(AuthContext)
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure><img src={Image} alt="Shoes" /></figure>
@@ -9,11 +14,22 @@ const ProductCard = ({ product, setOrders }) => {
                 <h2 className="card-title">{name}</h2>
                 <p>If a dog chews shoes whose shoes does he choose?</p>
                 <div className="card-actions justify-end">
-                    <label
-                        htmlFor="booking-modal"
-                        className="btn btn-primary text-white"
-                        onClick={() => setOrders(product)}
-                    >Book Now</label>
+                    {
+                        user ?
+                            <>
+                                <label
+                                    htmlFor="booking-modal"
+                                    className="btn btn-primary text-white"
+                                    onClick={() => setOrders(product)}
+                                >Book Now</label>
+                            </>
+                            :
+                            <>
+                                <Link to="/login"><button htmlFor="booking-modal"
+                                    className="btn btn-primary text-white"
+                                    onClick={() => setOrders(product)} >Book Now</button></Link>
+                            </>
+                    }
                 </div>
             </div>
         </div>
